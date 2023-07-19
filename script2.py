0356280446c3c75bbb3a2b0370176697d10df42b
Skalex Stuch <skalexstuch@me.com>
2023-07-19 19:03:38 +0800

    created a script V.2

---
+++ script2.py
@@ -0,0 +1,21 @@
+import time
+
+question = (
+    "",
+    "",
+    "",
+    "",
+    "",
+    "",
+    "",
+    "",
+    "",
+    "",
+)
+
+options = ()
+
+answer = ()
+guesses = []
+score = 0
+question_num = 0
